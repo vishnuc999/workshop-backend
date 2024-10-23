67053322b7eda8c1417c3d169c3ab28a995a89a9
vishal P K <vishalpk@G4TX0M7J3W-vishal.pk>
2024-10-23 20:12:36 +0530

    added separate folders for every functionality

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from 'src/controllers/app.controller';
+import { AppService } from 'src/services/app.service';
 
 @Module({
   imports: [],
